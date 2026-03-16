e7ae8fbefca513ab676aca3a3653d2c48e4101e5
travi01 <travi01@proton.me>
2026-03-16 21:05:39 +0700

    Update ExampleProvider.kt

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -1,16 +1,19 @@
 package com.example
 
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.TvType
+import com.lagardost.cloudstream3.SearchResponse
+import com.lagardost.cloudstream3.app
+import com.lagardost.cloudstream3.NewMovie.SearchResponse
 
 class LK21Provider : MainAPI() { 
     override var mainUrl = "https://tv9.lk21official.cc"
     override var name = "LK21 Official"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
+    override val supportedTypes = setOf(TvType.Movie)
 
     override suspend fun search(query: String): List<SearchResponse> {
-        val document = app.get("$mainUrl/?s=$query").document
+        val link = "$mainUrl/?s=$query"
+        val document = app.get(link).deocument
         return document.select("article.ui-grid-main").map {
             val title = it.selectFirst("h1.grid-title a")?.text() ?: ""
             val href = it.selectFirst("h1.grid-title a")?.attr("href") ?: ""
